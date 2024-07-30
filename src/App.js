94d4945fefd16537189a3679bfbccc637d4323ea
hg_yellow <jang010505@gmail.com>
2024-07-30 14:30:28 +0900

    -완-

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ function App() {
 
   const ask = 'Web Server와 Web Application Server의 차이점에 대해서 설명해주세요.';
 
+  const downloadRecording = () => {
+    const url = window.URL.createObjectURL(audioBlob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = '녹음 파일';
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+  }
+
   const askQuestion = async () => {
     try {
       const response = await fetch('https://api.openai.com/v1/chat/completions', {
@@ -27,7 +37,6 @@ function App() {
             role: 'user',
             content: `당신은 개발자 면접관이며, 다음 질문에 대해 면접자가 제대로 답변했는지 체크해야합니다.\n입력형식은 다음과 같습니다.\n면접 질문:  {질문 텍스트}\n면접 답변:  {답변 텍스트}\n해당 면접 질문에 대해 면접 답변이 정답인지, 틀렸다면 어느 부분이 틀렸는지 답변해주세요. 코드는 출력하지 않습니다.\n그리고 어느 부분이 틀린지 지적해주세요.\n 면접질문: ${ask} 면접답변: ${text}`
           }],
-          max_tokens: 150,
         })
       });
 
@@ -78,7 +87,7 @@ function App() {
   useEffect(() => {
     async function getMicrophoneAccess() {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/mp4' }); // m4a로 저장하기 위해 mimeType 설정
+      const mediaRecorder = new MediaRecorder(stream); // m4a로 저장하기 위해 mimeType 설정
       let audioChunks = [];
 
       mediaRecorder.addEventListener('dataavailable', (event) => {
@@ -88,7 +97,7 @@ function App() {
       });
 
       mediaRecorder.addEventListener('stop', () => {
-        const blob = new Blob(audioChunks, { type: 'audio/mp4' });
+        const blob = new Blob(audioChunks, { type: 'audio/wav' });
         const audioUrl = URL.createObjectURL(blob);
         setAudioURL(audioUrl);
         setAudioBlob(blob);
@@ -120,6 +129,7 @@ function App() {
       <div>{ask}</div>
       <button onClick={startRecording} disabled={recording}>Start Recording</button>
       <button onClick={stopRecording} disabled={!recording}>Stop Recording</button>
+      <button onClick={downloadRecording} disabled={!audioBlob}>Dwonload</button>
       {audioURL && <audio src={audioURL} controls />}
       {audioURL && <button onClick={transcribeAudio}>변환</button>}
       {text && <div>{text}</div>}
